74577358ea4071eb6b19f6192610739dc6104a42
Christian Kniep <christian@qnib.org>
2018-06-11 09:39:29 -0700

    adjust job-scripts

--- slurm/job-hello.sh
+++ slurm/job-hello.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --job-name=hello
-#SBATCH --output=/chome/slurm-%J.txt
+#SBATCH --output=/chome/cluser/slurm-%J.txt
 #SBATCH --ntasks=4
 
 mpirun --allow-run-as-root --mca btl_tcp_if_include eth0 -mca btl tcp,self /usr/local/bin/hello 2>/dev/null
